63ed47d9648db623399312c9f48f6ae327cd789e
wellhenrique <welissonh80@gmail.com>
2022-12-14 09:43:05 -0300

    chore: build app

--- dist/index.js
+++ dist/index.js
@@ -5,29 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
+const path_1 = __importDefault(require("path"));
 const prompts_1 = __importDefault(require("prompts"));
+const fs_1 = __importDefault(require("fs"));
 const program = new commander_1.Command();
 let projectPath = "";
-// program
-//   .command('create <project-name>')
-//   .description('Create a new project with the specified name')
-//   .action((projectName) => {
-//     // Create a new directory for the project
-//     fs.mkdirSync(projectName);
-//     // Create a package.json file for the project
-//     const packageJson = {
-//       name: projectName,
-//       version: '1.0.0',
-//       description: 'A new project created with the CLI',
-//     };
-//     fs.writeFileSync(path.join(projectName, 'package.json'), JSON.stringify(packageJson));
-//     // Create a README.md file for the project
-//     const readme = `# ${projectName}
-// A new project created with the CLI.`;
-//     fs.writeFileSync(path.join(projectName, 'README.md'), readme);
-//     console.log(`Successfully created project ${projectName}`);
-//   });
-// program.parse(process.argv);
 program
     .version("0.0.1")
     .description("Uma CLI para gerar modelos de projetos.")
@@ -40,9 +22,7 @@ program
 })
     .allowUnknownOption()
     .parse(process.argv);
-const options = program.opts();
 async function exec() {
-    console.log("projectName", projectPath);
     if (!projectPath) {
         const res = await (0, prompts_1.default)({
             type: "text",
@@ -59,6 +39,19 @@ async function exec() {
             projectPath = res.path.trim();
         }
     }
+    console.log("Criando projeto em: ", projectPath);
+    fs_1.default.mkdirSync(projectPath);
+    // Create a package.json file for the project
+    const packageJson = {
+        name: projectPath,
+        version: "1.0.0",
+        description: "A new project created with the CLI",
+    };
+    fs_1.default.writeFileSync(path_1.default.join(projectPath, "package.json"), JSON.stringify(packageJson));
+    // Create a README.md file for the project
+    const readme = `# ${projectPath}`;
+    fs_1.default.writeFileSync(path_1.default.join(projectPath, "README.md"), readme);
+    console.log(`Successfully created project ${projectPath}`);
 }
 exec();
 //# sourceMappingURL=index.js.map
